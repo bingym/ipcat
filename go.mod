c4a42509226610dad018f2749119f3c5ce3997dc
bingym <localstack@foxmail.com>
2021-04-07 10:41:00 +0800

    use ip.mcr.moe service

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module yinchuan
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3 // indirect
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/levigross/grequests v0.0.0-20190908174114-253788527a1a // indirect
+)
--- main.go
+++ main.go
@@ -3,13 +3,45 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/levigross/grequests"
 	"net/http"
 )
+
+type IPResp struct {
+	Status   int    `json:"status"`
+	Message  string `json:"message"`
+	Addr     string `json:"addr"`
+	Country  string `json:"country"`
+	Area     string `json:"area"`
+	Provider string `json:"provider"`
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, fmt.Sprintf("%s\n", c.ClientIP()))
+		clientIP := c.ClientIP()
+		response, err := grequests.Get(
+			"https://ip.mcr.moe",
+			&grequests.RequestOptions{Params: map[string]string{
+				"ip":  clientIP,
+				"db2": "",
+			}},
+		)
+		if err != nil {
+			c.String(http.StatusBadRequest, "查询IP信息失败: "+err.Error())
+			return
+		}
+		defer response.Close()
+		if response.StatusCode != 200 {
+			c.String(http.StatusInternalServerError, "查询IP信息失败: "+response.String())
+			return
+		}
+		var resp IPResp
+		if err := response.JSON(&resp); err != nil {
+			c.String(http.StatusInternalServerError, "查询IP信息失败: ", err.Error())
+			return
+		}
+		c.String(http.StatusOK, fmt.Sprintf("%s\n%s %s %s", clientIP, resp.Country, resp.Area, resp.Provider))
 	})
 	if err := r.Run(":9993"); err != nil {
 		panic(err)
